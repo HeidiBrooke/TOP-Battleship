6119bb702dc4b5cda6eb65c7d828eb412bed394e
H Brooke <hbrooke36@yahoo.com>
2023-03-08 09:21:22 -0800

    Add shorter ship creation test, prettifiy and lint

--- src/ship.js
+++ src/ship.js
@@ -1,134 +1,132 @@
-function validEntry(length, startPoint, direction, alpha){
-    if(direction == 'r'){
-        if ((startPoint[1] + length-1) > 9){
-            return false;
-        }
+function validEntry(length, startPoint, direction, alpha) {
+  if (direction === "r") {
+    if (startPoint[1] + length - 1 > 9) {
+      return false;
     }
-    if(direction == 'u'){
-        if((alpha.indexOf(startPoint[0])- length-1) < 0){
-            return false;
-        }
+  }
+  if (direction === "u") {
+    if (alpha.indexOf(startPoint[0]) - length - 1 < 0) {
+      return false;
     }
-    if(direction == 'd'){
-        if((alpha.indexOf(startPoint[0]) + length-1) > 9){
-            return false;
-        }
+  }
+  if (direction === "d") {
+    if (alpha.indexOf(startPoint[0]) + length - 1 > 9) {
+      return false;
     }
-    if(direction == 'l'){
-        if((startPoint[1]- length) < 0){
-            return false;
-        }
+  }
+  if (direction === "l") {
+    if (startPoint[1] - length < 0) {
+      return false;
     }
+  }
 
-    return true;
+  return true;
 }
 
-function right(length, startPoint){
-    let array = [];
-    for(let i = 0; i < length; i++){
-        let x;
-        let y;
-        x = (startPoint[0]);
-        y = startPoint[1];
-        startPoint[1]++;
-        array.push([x,y]);
-    }
-    return array;
+function right(length, startPoint) {
+  const array = [];
+  const x = startPoint[0];
+  let y = startPoint[1];
+  for (let i = 0; i < length; i += 1) {
+    array.push([x, y]);
+    y += 1;
+  }
+
+  return array;
 }
 
-function left(length, startPoint){
-    let array = [];
-    for(let i = 0; i < length; i++){
-        let x;
-        let y;
-        x = startPoint[0];
-        y = startPoint[1];
-        startPoint[1]--;
-        array.push([x,y]);
-    }
-    return array;
+function left(length, startPoint) {
+  const array = [];
+  const x = startPoint[0];
+  let y = startPoint[1];
+  for (let i = 0; i < length; i += 1) {
+    array.push([x, y]);
+    y -= 1;
+  }
+  return array;
 }
 
-function up(length, startPoint, alpha){
-    let array = [];
-    let index = alpha.indexOf(startPoint[0]);
-    for(let i = 0; i < length; i++){
-        let x;
-        let y;
-        x = alpha[index];
-        y = startPoint[1];
-        index--;
-        array.push([x,y]);
-    }
-    return array;
+function up(length, startPoint, alpha) {
+  const array = [];
+  let index = alpha.indexOf(startPoint[0]);
+  for (let i = 0; i < length; i += 1) {
+    const x = alpha[index];
+    const y = startPoint[1];
+    index -= 1;
+    array.push([x, y]);
+  }
+  return array;
 }
 
-function down(length, startPoint, alpha){
-    let array = [];
-    let index = alpha.indexOf(startPoint[0]);
-    for(let i = 0; i < length; i++){
-        let x;
-        let y;
-        x = alpha[index];
-        y = startPoint[1];
-        index++;
-        array.push([x,y]);
-    }
-    return array;
+function down(length, startPoint, alpha) {
+  const array = [];
+  let index = alpha.indexOf(startPoint[0]);
+  for (let i = 0; i < length; i += 1) {
+    const x = alpha[index];
+    const y = startPoint[1];
+    index += 1;
+    array.push([x, y]);
+  }
+  return array;
 }
 
 function generateCoordinates(length, startPoint, direction, alpha) {
-    if(direction == "r"){
-        return right(length, startPoint);
-    }
-    if(direction == 'u'){
-        return up(length, startPoint, alpha);
-    }
-    if(direction == 'd'){
-        return down(length, startPoint, alpha);
-    }
-    if(direction == 'l'){
-        return left(length, startPoint);
-    }
+  let coords;
+  if (direction === "r") {
+    coords = right(length, startPoint);
+  }
+  if (direction === "u") {
+    coords = up(length, startPoint, alpha);
+  }
+  if (direction === "d") {
+    coords = down(length, startPoint, alpha);
+  }
+  if (direction === "l") {
+    coords = left(length, startPoint);
+  }
+  return coords;
+}
+
+function generateHitLog(l) {
+  const array = [];
+  let i = 0;
+  while (i < l) {
+    array.push(0);
+    i += 1;
+  }
+  return array;
 }
 
 const ship = (l, startPoint, direction) => {
-    let alphas = 'abcdefghij';
-    let alpha = alphas.split('');
-    if(!validEntry(l, startPoint, direction, alpha)){
-        return 'Ship goes out of bounds. Try again'
-    };
-    const length = l;
-    const coords = generateCoordinates(l, startPoint, direction, alpha);
-    const hitLog = [0, 0, 0, 0, 0];
+  const alphas = "abcdefghij";
+  const alpha = alphas.split("");
+  if (!validEntry(l, startPoint, direction, alpha)) {
+    return "Ship goes out of bounds. Try again";
+  }
+  const length = l;
+  const coords = generateCoordinates(l, startPoint, direction, alpha);
+  const hitLog = generateHitLog(l);
 
-    const hit = (coordPair) => {
-        let index;
-        coords.forEach(coordinate => {
-            if((coordinate[0] == coordPair[0]) && (coordinate[1] == coordPair[1]) ){
-                index = coords.indexOf(coordinate);
-            }
-        })
-        if(index !== undefined){
-            hitLog[index] = 1;
-        }
-        return
+  const hit = (coordPair) => {
+    let index;
+    coords.forEach((coordinate) => {
+      if (coordinate[0] === coordPair[0] && coordinate[1] === coordPair[1]) {
+        index = coords.indexOf(coordinate);
+      }
+    });
+    if (index !== undefined) {
+      hitLog[index] = 1;
     }
+  };
 
-    const isSunk = () => {
-        if(!hitLog.includes(0)){
-            return true;
-        }
-        return false;
-        
+  const isSunk = () => {
+    if (!hitLog.includes(0)) {
+      return true;
     }
+    return false;
+  };
 
-    return {length, coords, hitLog, hit, isSunk};
-}
-
-
-
-
-
+  return { length, coords, hitLog, hit, isSunk };
+};
 
 export default ship;
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,98 +1,139 @@
-import ship  from './ship.js';
+/* eslint-disable no-undef */
+import ship from "./ship";
 
-const VL = ship(5, ['a', 0], 'r');
-VL.hit(['a', 0]);
-VL.hit(['a', 9]);
+const VL = ship(5, ["a", 0], "r");
+VL.hit(["a", 0]);
+VL.hit(["a", 9]);
 
-const VL2 = ship(5, ['a', 0], 'r');
-VL2.hit(['a', 0]);
-VL2.hit(['a', 3]);
+const VL2 = ship(5, ["a", 0], "r");
+VL2.hit(["a", 0]);
+VL2.hit(["a", 3]);
 
-const VL3 = ship(5, ['a', 0], 'r');
+const VL3 = ship(5, ["a", 0], "r");
 VL3.hitLog = [1, 1, 1, 1, 1];
-VL3.hit(['a', 0]);
-VL3.hit(['a', 1]);
-VL3.hit(['a', 2]);
-VL3.hit(['a', 3]);
-VL3.hit(['a', 4]);
+VL3.hit(["a", 0]);
+VL3.hit(["a", 1]);
+VL3.hit(["a", 2]);
+VL3.hit(["a", 3]);
+VL3.hit(["a", 4]);
 
 const VL3hits = VL3.hitLog;
 
 const testArray = [1, 0, 0, 0, 0];
 
-test('make long ship in bounds right', () => {
+test("make long ship in bounds right", () => {
   expect(ship(5, ["a", 5], "r")).toMatchObject({
     length: 5,
-    coords: [["a",5], ["a",6], ["a",7], ["a",8], ["a", 9]],
+    coords: [
+      ["a", 5],
+      ["a", 6],
+      ["a", 7],
+      ["a", 8],
+      ["a", 9],
+    ],
     hitLog: [0, 0, 0, 0, 0],
   });
 });
 
-test('make long ship in bounds up', () => {
-    expect(ship(5, ["h", 5], "u")).toMatchObject({
-      length: 5,
-      coords: [["h",5], ["g",5], ["f",5], ["e",5], ["d",5]],
-      hitLog: [0, 0, 0, 0, 0],
-    });
+test("make long ship in bounds up", () => {
+  expect(ship(5, ["h", 5], "u")).toMatchObject({
+    length: 5,
+    coords: [
+      ["h", 5],
+      ["g", 5],
+      ["f", 5],
+      ["e", 5],
+      ["d", 5],
+    ],
+    hitLog: [0, 0, 0, 0, 0],
   });
+});
 
-test('make long ship in bounds down', () => {
-    expect(ship(5, ["f", 5], "d")).toMatchObject({
-      length: 5,
-      coords: [["f",5], ["g",5], ["h",5], ["i",5], ["j",5]],
-      hitLog: [0, 0, 0, 0, 0],
-    });
+test("make long ship in bounds down", () => {
+  expect(ship(5, ["f", 5], "d")).toMatchObject({
+    length: 5,
+    coords: [
+      ["f", 5],
+      ["g", 5],
+      ["h", 5],
+      ["i", 5],
+      ["j", 5],
+    ],
+    hitLog: [0, 0, 0, 0, 0],
   });
+});
 
-test('make long ship in bounds left', () => {
-    expect(ship(5, ["f", 5], "l")).toMatchObject({
-      length: 5,
-      coords: [["f",5], ["f",4], ["f",3], ["f",2], ["f",1]],
-      hitLog: [0, 0, 0, 0, 0],
-    });
+test("make long ship in bounds left", () => {
+  expect(ship(5, ["f", 5], "l")).toMatchObject({
+    length: 5,
+    coords: [
+      ["f", 5],
+      ["f", 4],
+      ["f", 3],
+      ["f", 2],
+      ["f", 1],
+    ],
+    hitLog: [0, 0, 0, 0, 0],
   });
+});
 
-test('make long ship out of bounds up', () => {
-    expect(ship(5, ["a",0], "u")).toBe('Ship goes out of bounds. Try again');
-  });
+test("make long ship out of bounds up", () => {
+  expect(ship(5, ["a", 0], "u")).toBe("Ship goes out of bounds. Try again");
+});
 
-test('make long ship out of bounds left', () => {
-    expect(ship(5, ["a",0], "l")).toBe('Ship goes out of bounds. Try again');
-  });
-test('make long ship out of bounds right', () => {
-    expect(ship(5, ["a", 7], "r")).toBe('Ship goes out of bounds. Try again');
-  });
-test('make long ship out of bounds down', () => {
-    expect(ship(5, ["g",9], "d")).toBe('Ship goes out of bounds. Try again');
-  });
+test("make long ship out of bounds left", () => {
+  expect(ship(5, ["a", 0], "l")).toBe("Ship goes out of bounds. Try again");
+});
+test("make long ship out of bounds right", () => {
+  expect(ship(5, ["a", 7], "r")).toBe("Ship goes out of bounds. Try again");
+});
+test("make long ship out of bounds down", () => {
+  expect(ship(5, ["g", 9], "d")).toBe("Ship goes out of bounds. Try again");
+});
 
-test('VL takes hit in [a, 1]', () => {
-    expect(VL.hitLog).toMatchObject(
-        testArray);
-  });
+test("VL takes hit in [a, 1]", () => {
+  expect(VL.hitLog).toMatchObject(testArray);
+});
 
-test('VL takes miss in [a, 9]', () => {
-    expect(VL.hitLog).toMatchObject(
-        [1, 0, 0, 0, 0]);
-  });
+test("VL takes miss in [a, 9]", () => {
+  expect(VL.hitLog).toMatchObject([1, 0, 0, 0, 0]);
+});
 
+test("VL2 takes hit in [a, 3]", () => {
+  expect(VL2.hitLog).toMatchObject([1, 0, 0, 1, 0]);
+});
 
-test('VL2 takes hit in [a, 3]', () => {
-    expect(VL2.hitLog).toMatchObject(
-        [1, 0, 0, 1, 0]);
-  });
+test("VL2 is sunk", () => {
+  expect(VL2.isSunk()).toBe(false);
+});
 
-test('VL2 is sunk', () => {
-    expect(VL2.isSunk()).toBe(
-        false);
-  });
+test("VL3 is sunk", () => {
+  expect(VL3.isSunk()).toBe(true);
+});
 
-test('VL3 is sunk', () => {
-    expect(VL3.isSunk()).toBe(
-        true);
-  });
+test("VL3 hit log", () => {
+  expect(VL3hits).toMatchObject([1, 1, 1, 1, 1]);
+});
 
-test('VL3 hit log', () => {
-    expect(VL3hits).toMatchObject(
-        [1, 1, 1, 1, 1]);
+test("make medium ship in bounds left", () => {
+  expect(ship(3, ["f", 5], "l")).toMatchObject({
+    length: 3,
+    coords: [
+      ["f", 5],
+      ["f", 4],
+      ["f", 3],
+    ],
+    hitLog: [0, 0, 0],
   });
+});
+
+test("make short ship in bounds left", () => {
+  expect(ship(2, ["f", 5], "l")).toMatchObject({
+    length: 2,
+    coords: [
+      ["f", 5],
+      ["f", 4],
+    ],
+    hitLog: [0, 0],
+  });
+});
